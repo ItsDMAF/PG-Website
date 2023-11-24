86edb468dae43f9cd357364cb7e922743e5aafed
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-11-24 13:28:04 +0100

    Correction de colores y arreglo de formato(faltan partes que mejorar aun)

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function() {
+m
+function redirectToMenu(location) {
+    window.location.href = location + ".html";
+}enu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
-
-function redirectToMenu(location) {
-    window.location.href = location + ".html";
-}
