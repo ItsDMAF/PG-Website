0f4db6917e39a49e6bf3c8eb3d784ef3bd585f2f
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-11-25 03:57:37 +0100

    Correcion de imagenes y codigo de js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-m
-function redirectToMenu(location) {
-    window.location.href = location + ".html";
-}enu.addEventListener('click', function() {
+menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function redirectToMenu(location) {
+    window.location.href = location + ".html";
+}
