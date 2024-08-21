79635a1c67a06c63a3af5a43630971f03ad623f3
ItsDMAF <dmaf.andinofiallos@gmail.com>
2024-08-22 00:35:00 +0200

    Intentando mejorar el menu en general

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ function redirectToMenu(location) {
     window.location.href = location + ".html";
 }
 
+document.getElementById('combo-image').addEventListener('click', function() {
+    const imageSection = document.getElementById('image-section');
+    const comboItems = document.getElementById('combo-items');
+    
+    imageSection.style.display = 'none';
+    
+    comboItems.style.display = 'grid';
+});
+
+document.getElementById('back-button').addEventListener('click', function() {
+    const imageSection = document.getElementById('image-section');
+    const comboItems = document.getElementById('combo-items');
+    
+    imageSection.style.display = 'block';
+    
+    comboItems.style.display = 'none';
+});
