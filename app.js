a3eb2d3cb7641f94860dce01ed58c49ff11cccc1
ItsDMAF <dmaf.andinofiallos@gmail.com>
2024-08-29 06:45:37 +0200

    correcion de la barra de opciones

--- app.js
+++ app.js
@@ -5,27 +5,3 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
-
-function redirectToMenu(location) {
-    window.location.href = location + ".html";
-}
-
-document.querySelectorAll('.combo-image').forEach(function(image, index) {
-    image.addEventListener('click', function() {
-        const imageSection = document.getElementById(`image-section-${index + 1}`);
-        const comboItems = document.getElementById(`combo-items-${index + 1}`);
-
-        imageSection.style.display = 'none';
-        comboItems.style.display = 'grid';
-    });
-});
-
-document.querySelectorAll('.back-button').forEach(function(button, index) {
-    button.addEventListener('click', function() {
-        const imageSection = document.getElementById(`image-section-${index + 1}`);
-        const comboItems = document.getElementById(`combo-items-${index + 1}`);
-
-        imageSection.style.display = 'block';
-        comboItems.style.display = 'none';
-    });
-});
